c4e0a26e22ac2c3c3e12cf834a576e84ca53f88e
Mateusz Grabara <mateusz@grabara.eu>
2019-07-24 23:01:46 +0100

    Added input validation for Person and Skill

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,14 +1,18 @@
 class Person < ApplicationRecord
-    has_many :skill_taggings
-    has_many :skills, through: :skill_taggings
+  has_many :skill_taggings
+  has_many :skills, through: :skill_taggings
 
-    def all_skills=(names)
-        self.skills = names.split(",").map do |name|
-            Skill.where(name: name.strip).first_or_create!
-        end
-      end
-      
-    def all_skills
-      self.skills.map(&:name).join(", ")
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :email, presence: true, length: { minimum: 5 }, uniqueness: { case_sensitive: false }
+
+  def all_skills=(names)
+    self.skills = names.split(",").map do |name|
+      Skill.where(name: name.strip).first_or_create!
     end
+  end
+      
+  def all_skills
+    self.skills.map(&:name).join(", ")
+  end
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,5 @@
 class Skill < ApplicationRecord
     has_many :skill_taggings
     has_many :people, through: :skill_taggings
+    validates :skill_name, presence: true, uniqueness: { case_sensitive: false }
 end
