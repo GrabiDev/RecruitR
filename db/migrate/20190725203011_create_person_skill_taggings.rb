8856ca37b989cc075fb0ab4ce013acc47b106989
Mateusz Grabara <mateusz@grabara.eu>
2019-07-25 22:43:53 +0100

    Added ON_DELETE behaviour to data model

--- db/migrate/20190725203011_create_person_skill_taggings.rb
+++ db/migrate/20190725203011_create_person_skill_taggings.rb
@@ -1,8 +1,8 @@
 class CreatePersonSkillTaggings < ActiveRecord::Migration[5.2]
   def change
     create_table :person_skill_taggings do |t|
-      t.belongs_to :person, foreign_key: true
-      t.belongs_to :skill, foreign_key: true
+      t.belongs_to :person, foreign_key: { on_delete: :cascade }
+      t.belongs_to :skill, foreign_key: { on_delete: :cascade }
 
       t.timestamps
     end
--- db/migrate/20190725204103_create_position_skill_taggings.rb
+++ db/migrate/20190725204103_create_position_skill_taggings.rb
@@ -1,8 +1,8 @@
 class CreatePositionSkillTaggings < ActiveRecord::Migration[5.2]
   def change
     create_table :position_skill_taggings do |t|
-      t.belongs_to :position, foreign_key: true
-      t.belongs_to :skill, foreign_key: true
+      t.belongs_to :position, foreign_key: { on_delete: :cascade }
+      t.belongs_to :skill, foreign_key: { on_delete: :cascade }
 
       t.timestamps
     end
