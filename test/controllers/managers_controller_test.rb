335cdf3f3e05d62dc984ec917f805948247f59d6
Mateusz Grabara <mateusz@grabara.eu>
2019-07-25 21:50:17 +0100

    Cleared up tests as per guide

--- test/controllers/managers_controller_test.rb
+++ test/controllers/managers_controller_test.rb
@@ -1,24 +1,4 @@
 require 'test_helper'
 
 class ManagersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get managers_index_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get managers_new_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get managers_show_url
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get managers_edit_url
-    assert_response :success
-  end
-
 end
