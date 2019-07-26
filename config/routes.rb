7714ad337ff0eba352c0b984f2ec797c30624dda
Mateusz Grabara <mateusz@grabara.eu>
2019-07-26 18:17:36 +0100

    Deleted Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,9 @@ Rails.application.routes.draw do
   get 'skills/new'
   get 'managers/index'
   get 'managers/new'
-  get 'dashboard/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :skills, :managers, :candidates, :interviews, :scorecards, :positions
 
-  root 'dashboard#index'
+  root 'positions#index'
 end
