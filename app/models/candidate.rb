ef885be794a8d9e6266943189deec21761855f4e
Mateusz Grabara <mateusz@grabara.eu>
2019-07-26 21:59:46 +0100

    Added callback to destroy Interview before Person

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,4 +1,9 @@
 class Candidate < Person
     belongs_to :position
     has_one :interview
+    before_destroy :destroy_interview
+
+    def destroy_interview
+      interview.destroy
+    end
 end
--- db/migrate/20190726074122_create_manager_interview_taggings.rb
+++ db/migrate/20190726074122_create_manager_interview_taggings.rb
@@ -1,10 +1,12 @@
 class CreateManagerInterviewTaggings < ActiveRecord::Migration[5.2]
   def change
     create_table :manager_interview_taggings do |t|
-      t.belongs_to :interview, foreign_key: true
+      t.belongs_to :interview, foreign_key: false
       t.belongs_to :manager, foreign_key: false
 
       t.timestamps
+
+      add_foreign_key :interview, :manager, on_delete: :cascade
     end
   end
 end
